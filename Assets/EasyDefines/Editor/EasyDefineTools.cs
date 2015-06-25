96ff93f217c62f0cba281b2ab606035e4306794c
Chase Cobb <chase@8bitroots.com>
2015-06-25 16:30:03 -0700

    --blocking out functionality

--- Assets/EasyDefines/Editor/EasyDefineTools.cs
+++ Assets/EasyDefines/Editor/EasyDefineTools.cs
@@ -1,15 +1,102 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.IO;
+using System.Collections.Generic;
 
-public class EasyDefineTools : MonoBehaviour {
+public class EasyDefineTools
+{
+    public static readonly string SYNC_FILE_LOCATION = "/EasyDefines/EasyDefines.txt";
+    public static readonly string INACTIVE_DEFINE_PREFIX = ":0:0:0";
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public enum DefineType
+    {
+        C_SHARP = 1,
+        C_SHARP_EDITOR = 2,
+        UNITYSCRIPT = 4
+    }
+
+    public static void AddDefine(string define)
+    {
+        string filePath = Application.dataPath + SYNC_FILE_LOCATION;
+        if (!File.Exists(filePath))
+        {
+            File.Create(filePath);
+        }
+
+        string[] allLines = File.ReadAllLines();
+
+        // does this define already exist?
+        foreach(string line in allLines)
+        {
+            if(line.Contains(define))
+            {
+                Debug.Log("This define already exists");
+                return;
+            }
+        }
+
+        File.AppendAllText(filePath, define + INACTIVE_DEFINE_PREFIX + Environment.NewLine);
+    }
+
+    public static void RemoveDefine(string define)
+    {
+        string filePath = Application.dataPath + SYNC_FILE_LOCATION;
+        if (!File.Exists(filePath))
+        {
+            File.Create(filePath);
+        } 
+        else
+        {
+            List<string> allLines = new List<string>(File.ReadAllLines());
+            string lineContainingDefine = string.Empty;
+
+            foreach(string line in allLines)
+            {
+                if(line.Contains(define))
+                {
+                    lineContainingDefine = line;
+                }
+            }
+
+            if(lineContainingDefine != string.Empty)
+            {
+                allLines.Remove(lineContainingDefine);
+                File.WriteAllLines(filePath, allLines.ToArray());
+            }
+            else
+            {
+                Debug.Log("Can't remove define, not found");
+            }
+        }
+    }
+
+    public static void SetDefineActive(string define,bool isActive, DefineType defineType)
+    {
+    }
+
+    public static void ForceRecompile()
+    {
+    }
+
+    public static void ClearDefines()
+    {
+    }
+
+    private static void SyncDefinesToRSP()
+    {
+        string filePath = Application.dataPath + SYNC_FILE_LOCATION;
+        if (!File.Exists(filePath))
+        {
+            File.Create(filePath);
+        }
+
+
+        
+        // TODO : parse sync file and add active defines to respective rsp files
+
+    }
+
+    private static void SyncDefinesFromRSP()
+    {
+    }
 }
--- Assets/EasyDefines/Editor/EasyDefinesWindow.cs
+++ Assets/EasyDefines/Editor/EasyDefinesWindow.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEditor;
 
-public class EasyDefinesWindow : MonoBehaviour {
+public class EasyDefinesWindow : EditorWindow
+{
+    private static bool m_autoRecompile = false;
 
-	// Use this for initialization
-	void Start () {
-	
+	[MenuItem("Window/Easy Defines")]
+	public static void ShowWindow()
+	{
+		EasyDefinesWindow definesWindow = EditorWindow.GetWindow<EasyDefinesWindow> ("Easy Defines");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnGUI()
+	{
+        bool changeDetected = false;
+        bool recompileButtonPressed = false;
+		// TODO : DRAW WINDOW
+
+		// C# - smcs.rsp
+		// C# Editor - gmcs.rsp
+		// UnityScript = us.rsp
+
+        // draw current list of entries with checkboxes to activate for a specific rsp
+            // right click contextual menu to remove
+
+        // textfield for adding new entries
+
+        if ((changeDetected && m_autoRecompile) || recompileButtonPressed)
+        {
+            EasyDefineTools.ForceRecompile();
+        }
 	}
+//“-define:UNITY_DEBUG”
+// unsafe ?
 }
